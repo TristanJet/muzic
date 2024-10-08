8197169e6c9c8f80659dcbf0aea4d35437c4b4de
TristanJet <tristanjetlay1@gmail.com>
2024-10-08 17:06:10 +0300

    almost hello world working

--- src/main.zig
+++ src/main.zig
@@ -5,16 +5,21 @@ const io = std.io;
 const mem = std.mem;
 const os = std.os;
 
+var tty: fs.File = undefined;
+var size: Size = undefined;
+
 pub fn main() !void {
-    var tty: fs.File = try fs.cwd().openFile(
+    tty = try fs.cwd().openFile(
         "/dev/tty",
         .{ .mode = fs.File.OpenMode.read_write },
     );
     defer tty.close();
 
+    size = try getSize();
+
     var original: os.linux.termios = undefined;
     _ = os.linux.tcgetattr(tty.handle, &original);
-    var raw = original;
+    var raw: os.linux.termios = original;
     inline for (.{ "ECHO", "ICANON", "ISIG", "IEXTEN" }) |flag| {
         @field(raw.lflag, flag) = false;
     }
@@ -26,6 +31,7 @@ pub fn main() !void {
     _ = os.linux.tcsetattr(tty.handle, .FLUSH, &raw);
 
     while (true) {
+        _ = try render();
         var buffer: [1]u8 = undefined;
         _ = try tty.read(&buffer);
         if (buffer[0] == 'q') {
@@ -39,4 +45,46 @@ pub fn main() !void {
             debug.print("input: {} {s}\r\n", .{ buffer[0], buffer });
         }
     }
+}
+
+fn clear(writer: anytype) !void {
+    try writer.writeAll("\x1B[2J");
+}
+
+fn render() !void {
+    const writer = tty.writer();
+    try clear(writer);
+    try writeLine(writer, "Hello, World!!", (size.height / 2), size.width);
+}
+
+fn moveCursor(writer: anytype, row: usize, col: usize) !void {
+    _ = try writer.print("\x1B[{};{}H", .{ row + 1, col + 1 });
+}
+
+fn writeLine(writer: anytype, txt: []const u8, y: usize, width: usize) !void {
+    try moveCursor(writer, y, 0);
+    try writer.writeAll(txt);
+    try writer.writeByteNTimes(' ', width - txt.len);
+}
+
+fn attributeReset(writer: anytype) !void {
+    try writer.writeAll("\x1B[0m");
+}
+
+fn blueBackground(writer: anytype) !void {
+    try writer.writeAll("\x1B[44m");
+}
+
+const Size = struct { width: usize, height: usize };
+
+fn getSize() !Size {
+    var win_size = mem.zeroes(os.linux.winsize);
+    const err = os.linux.ioctl(tty.handle, os.linux.T.IOCGWINSZ, @intFromPtr(&win_size));
+    if (std.posix.errno(err) != .SUCCESS) {
+        return std.posix.unexpectedErrno(os.linux.E.init(err));
+    }
+    return Size{
+        .height = win_size.ws_row,
+        .width = win_size.ws_col,
+    };
 }
