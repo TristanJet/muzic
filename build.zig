c16efe79eb0c54e9ea735858f9e44dc0c8debf4f
Tristan Jet <tristanjetlay1@gmail.com>
2026-01-07 05:29:49 +0200

    update build.zig and build.zig.zon

--- build.zig
+++ build.zig
@@ -8,11 +8,19 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = if (optimize == .ReleaseFast) "muzic" else "out",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            // b.createModule defines a new module just like b.addModule but,
+            // unlike b.addModule, it does not expose the module to consumers of
+            // this package, which is why in this case we don't have to give it a name.
+            .root_source_file = b.path("src/main.zig"),
+            // Target and optimization levels must be explicitly wired in when
+            // defining an executable or library (in the root module), and you
+            // can also hardcode a specific target for an executable or library
+            // definition if desireable (e.g. firmware for embedded devices).
+            .target = target,
+            .optimize = optimize,
+        }),
     });
-
     exe.root_module.addImport("DisplayWidth", zg.module("DisplayWidth"));
     exe.root_module.addImport("code_point", zg.module("code_point"));
     exe.root_module.addImport("ascii", zg.module("ascii"));
